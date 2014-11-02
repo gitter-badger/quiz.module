07d674dae0d7b8f71cc3b6a635adaace15257afb
Andy Truong <thehongtt@gmail.com>
2014-11-02 16:05:11 +0700

    Simplify NodeValidateHelper::execute().

--- src/Helper/Node/NodeValidateHelper.php
+++ src/Helper/Node/NodeValidateHelper.php
@@ -24,49 +24,59 @@ class NodeValidateHelper {
       }
     }
 
-    if (isset($node->resultoptions) && count($node->resultoptions) > 0) {
-      $taken_values = array();
-      $num_options = 0;
-      foreach ($node->resultoptions as $option) {
-        if (!empty($option['option_name'])) {
-          $num_options++;
-          if (empty($option['option_summary'])) {
-            form_set_error('option_summary', t('Range has no summary text.'));
-          }
-          if ($node->pass_rate && (isset($option['option_start']) || isset($option['option_end']))) {
-
-            // Check for a number between 0-100.
-            foreach (array('option_start' => 'start', 'option_end' => 'end') as $bound => $bound_text) {
-              if (!_quiz_is_int($option[$bound], 0, 100)) {
-                form_set_error($bound, t('The range %start value must be a number between 0 and 100.', array('%start' => $bound_text)));
-              }
-            }
-
-            // Check that range end >= start.
-            if ($option['option_start'] > $option['option_end']) {
-              form_set_error('option_start', t('The start must be less than the end of the range.'));
-            }
-
-            // Check that range doesn't collide with any other range.
-            $option_range = range($option['option_start'], $option['option_end']);
-            if ($intersect = array_intersect($taken_values, $option_range)) {
-              form_set_error('option_start', t('The ranges must not overlap each other. (%intersect)', array('%intersect' => implode(',', $intersect))));
-            }
-            else {
-              $taken_values = array_merge($taken_values, $option_range);
-            }
-          }
-        }
-        elseif (!$this->isEmptyHTML($option['option_summary']['value'])) {
-          form_set_error('option_summary', t('Range has a summary, but no name.'));
-        }
-      }
-    }
+    $this->validateResultOptions($node);
 
     if ($node->allow_jumping && !$node->allow_skipping) {
       // @todo when we have pages of questions, we have to check that jumping is
       // not enabled, and randomization is not enabled unless there is only 1 page
       form_set_error('allow_skipping', t('If jumping is allowed, skipping must also be allowed.'));
+    }
+  }
+
+  private function validateResultOptions($node) {
+    if (!isset($node->resultoptions) || !count($node->resultoptions)) {
+      return;
+    }
+
+    $taken_values = array();
+    $num_options = 0;
+    foreach ($node->resultoptions as $option) {
+      $this->validateResultOption($node, $option, $taken_values, $num_options);
+    }
+  }
+
+  private function validateResultOption($node, $option, &$taken_values, &$num_options) {
+    if (empty($option['option_name']) && !$this->isEmptyHTML($option['option_summary']['value'])) {
+      form_set_error('option_summary', t('Range has a summary, but no name.'));
+      return;
+    }
+
+    $num_options++;
+    if (empty($option['option_summary'])) {
+      form_set_error('option_summary', t('Range has no summary text.'));
+    }
+
+    if ($node->pass_rate && (isset($option['option_start']) || isset($option['option_end']))) {
+      // Check for a number between 0-100.
+      foreach (array('option_start' => 'start', 'option_end' => 'end') as $bound => $bound_text) {
+        if (!_quiz_is_int($option[$bound], 0, 100)) {
+          form_set_error($bound, t('The range %start value must be a number between 0 and 100.', array('%start' => $bound_text)));
+        }
+      }
+
+      // Check that range end >= start.
+      if ($option['option_start'] > $option['option_end']) {
+        form_set_error('option_start', t('The start must be less than the end of the range.'));
+      }
+
+      // Check that range doesn't collide with any other range.
+      $option_range = range($option['option_start'], $option['option_end']);
+      if ($intersect = array_intersect($taken_values, $option_range)) {
+        form_set_error('option_start', t('The ranges must not overlap each other. (%intersect)', array('%intersect' => implode(',', $intersect))));
+      }
+      else {
+        $taken_values = array_merge($taken_values, $option_range);
+      }
     }
   }
 
