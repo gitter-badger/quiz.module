f7a9c8f42cae586f7b48a53734d8adf57160f385
Andy Truong <thehongtt@gmail.com>
2014-11-02 09:38:03 +0700

    Clean up quiz_confirm.js

--- misc/js/quiz_confirm.js
+++ misc/js/quiz_confirm.js
@@ -1,15 +1,16 @@
-(function ($) {
+(function ($, Drupal) {
 
   Drupal.behaviors.quizAnswerConfirm = {
-    attach: function (context, settings) {
-      $('form.quiz-answer-confirm #edit-submit').once(function () {
-        var $form = $(this);
-        $form.submit(function () {
-          // Return false to avoid submitting if user aborts
-          return confirm($form.data('confirm-message'));
+    attach: function (context) {
+      $('form.quiz-answer-confirm #edit-submit', context).once(function () {
+        var message = $(this).data('confirm-message');
+
+        // Return false to avoid submitting if user aborts
+        $(this).submit(function () {
+          return confirm(message);
         });
       });
     }
   };
 
-})(jQuery);
+})(jQuery, Drupal);
