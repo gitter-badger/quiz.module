55084a9a0fdcf35bca50aa6b46f4dea11ac6ce7e
Andy Truong <thehongtt@gmail.com>
2014-11-10 09:37:45 +0700

    misc

--- misc/js/quiz_take.js
+++ misc/js/quiz_take.js
@@ -1,19 +1,21 @@
-(function ($) {
+(function ($, Drupal) {
+
   Drupal.behaviors.quizQuestionBrowserBehavior = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
       //  $('div.checkbox div.form-item').hide();
       var $cell = $('div.mark-doubtful');
       var $checkbox = $(':checkbox', $cell);
       var $switch = $('.toggle', $cell);
       if ($checkbox.is(':checked')) {
-	$switch.toggleClass('off');
+        $switch.toggleClass('off');
       }
-      $switch.click(function() {
-	$checkbox.click();
+      $switch.click(function () {
+        $checkbox.click();
       });
-      $checkbox.click(function() {
-	$switch.toggleClass('off');
+      $checkbox.click(function () {
+        $switch.toggleClass('off');
       });
     }
   };
-})(jQuery);
+
+})(jQuery, Drupal);
