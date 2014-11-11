11b7f424161336a3434c0f52da148c463feb1063
Andy Truong <thehongtt@gmail.com>
2014-11-11 16:56:45 +0700

    Define basic structure for /quiz-question/add/%

--- modules/quiz_question/src/Entity/QuestionUIController.php
+++ modules/quiz_question/src/Entity/QuestionUIController.php
@@ -15,7 +15,7 @@ class QuestionUIController extends \EntityDefaultUIController {
 
     // Change path from /admin/content/quiz/add -> /quizz/add
     unset($items['admin/content/quiz-questions/add']);
-    $items['quiz-question/add'] = array(
+    $items['quiz_question/add'] = array(
         'title'            => 'Add question',
         'access callback'  => 'entity_access',
         'access arguments' => array('create', 'quiz_question'),
@@ -23,6 +23,19 @@ class QuestionUIController extends \EntityDefaultUIController {
         'file'             => 'quiz_question.pages.inc',
         'page callback'    => 'quiz_question_adding_landing_page',
     );
+
+    foreach (array_keys(quiz_question_get_types()) as $name) {
+      $items['quiz-question/add/' . $name] = array(
+          'title callback'   => 'entity_ui_get_action_title',
+          'title arguments'  => array('add', 'quiz_question'),
+          'access callback'  => 'entity_access',
+          'access arguments' => array('create', 'quiz_question'),
+          'page callback'    => 'quiz_question_adding_page',
+          'page arguments'   => array($name),
+          'file path'        => drupal_get_path('module', 'quiz_question'),
+          'file'             => 'quiz_question.pages.inc',
+      );
+    }
 
     return $items;
   }
