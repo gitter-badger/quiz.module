d06fa72a7a93cc55430f95dd0a50b7e6994e7e84
Andy Truong <thehongtt@gmail.com>
2014-11-11 11:59:10 +0700

    Add missing callback functions.

--- modules/quiz_question/quiz_question.todo.php
+++ modules/quiz_question/quiz_question.todo.php
@@ -10,3 +10,26 @@ function quiz_question_type_load($name) {
   $types = entity_load_multiple_by_name('quiz_question_type', array($name));
   return isset($types[$name]) ? $types[$name] : NULL;
 }
+
+function quiz_question_type_access() {
+  return TRUE;
+}
+
+/**
+ * Access callback for question entity.
+ *
+ * @param string $op
+ * @param string $type
+ * @param stdClass $account
+ */
+function quiz_question_access_callback() {
+  switch ($op) {
+    case 'create':
+      return user_access('create question content', $account);
+    case 'update':
+      return user_access('edit any question content', $account);
+    case 'view':
+      return user_access('access question', $account);
+  }
+  return TRUE;
+}
