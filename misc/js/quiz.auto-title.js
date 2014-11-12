5741bbaa424621afbc4add52ba26d15b7d43bc0a
Andy Truong <thehongtt@gmail.com>
2014-11-12 17:05:03 +0700

    Update quiz.auto-title.js to work with question entity's default body field.

--- misc/js/quiz.auto-title.js
+++ misc/js/quiz.auto-title.js
@@ -1,30 +1,31 @@
 (function ($, Drupal) {
   var maxLength = Drupal.settings.quiz_max_length;
+  var body_query = '#edit-body textarea:eq(1), #edit-quiz-question-body-und-0-value';
 
   function quizStripTags(str) {
     return str.replace(/<\/?[^>]+>/gi, '');
   }
 
   function quizUpdateTitle() {
-    var body = $("#edit-body textarea:eq(1)").val();
+    var body = $(body_query).val();
     if (quizStripTags(body).length > maxLength) {
-      $("#edit-title").val(quizStripTags(body).substring(0, maxLength - 3) + "…");
+      $('#edit-title').val(quizStripTags(body).substring(0, maxLength - 3) + "…");
     }
     else {
-      $("#edit-title").val(quizStripTags(body).substring(0, maxLength));
+      $('#edit-title').val(quizStripTags(body).substring(0, maxLength));
     }
   }
 
   $(document).ready(function () {
-    $("#edit-body textarea").keyup(quizUpdateTitle);
+    $(body_query).keyup(quizUpdateTitle);
 
     // Do not use auto title if a title already has been set
-    if ($("#edit-title").val().length > 0) {
-      $("#edit-body textarea:eq(1)").unbind("keyup", quizUpdateTitle);
+    if ($('#edit-title').val().length > 0) {
+      $(body_query).unbind("keyup", quizUpdateTitle);
     }
 
-    $("#edit-title").keyup(function () {
-      $("#edit-body textarea:eq(1)").unbind("keyup", quizUpdateTitle);
+    $('#edit-title').keyup(function () {
+      $(body_query).unbind("keyup", quizUpdateTitle);
     });
   });
 
