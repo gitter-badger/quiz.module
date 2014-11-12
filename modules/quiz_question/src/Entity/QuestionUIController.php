f475b161a10213f6006c1588e0c63011f3736bda
Andy Truong <thehongtt@gmail.com>
2014-11-12 16:21:42 +0700

    - in path instead of _.

--- modules/quiz_question/src/Entity/QuestionUIController.php
+++ modules/quiz_question/src/Entity/QuestionUIController.php
@@ -15,7 +15,7 @@ class QuestionUIController extends \EntityDefaultUIController {
 
     // Change path from /admin/content/quiz/add -> /quizz/add
     unset($items['admin/content/quiz-questions/add']);
-    $items['quiz_question/add'] = array(
+    $items['quiz-question/add'] = array(
         'title'            => 'Add question',
         'access callback'  => 'entity_access',
         'access arguments' => array('create', 'quiz_question'),
@@ -25,7 +25,7 @@ class QuestionUIController extends \EntityDefaultUIController {
     );
 
     foreach (array_keys(quiz_question_get_types()) as $name) {
-      $items['quiz-question/add/' . $name] = array(
+      $items['quiz-question/add/' . str_replace('_', '-', $name)] = array(
           'title callback'   => 'entity_ui_get_action_title',
           'title arguments'  => array('add', 'quiz_question'),
           'access callback'  => 'entity_access',
