67190c8e399dfa14b3bee24807dedddef3f00f79
Andy Truong <thehongtt@gmail.com>
2014-11-13 15:55:49 +0700

    Call question plugin method on entity save.

--- modules/quiz_question/src/Entity/QuestionController.php
+++ modules/quiz_question/src/Entity/QuestionController.php
@@ -2,13 +2,19 @@
 
 namespace Drupal\quiz_question\Entity;
 
+use DatabaseTransaction;
 use EntityAPIController;
 
 class QuestionController extends EntityAPIController {
 
-  public function save($entity, \DatabaseTransaction $transaction = NULL) {
-    $return = parent::save($entity, $transaction);
-    return $return;
+  /**
+   * @param Question $question
+   * @param DatabaseTransaction $transaction
+   */
+  public function save($question, DatabaseTransaction $transaction = NULL) {
+    $is_new = parent::save($question, $transaction);
+    $question->getPlugin()->save($is_new);
+    return $is_new;
   }
 
 }
