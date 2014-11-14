2d03ba26aae3c7f134f497ae97505f304aea5cde
Andy Truong <thehongtt@gmail.com>
2014-11-14 11:41:23 +0700

    Provide metadata for question entity.

--- modules/quiz_question/src/Entity/QuestionMetadataController.php
+++ modules/quiz_question/src/Entity/QuestionMetadataController.php
@@ -8,9 +8,15 @@ class QuestionMetadataController extends EntityDefaultMetadataController {
 
   public function entityPropertyInfo() {
     $info = parent::entityPropertyInfo();
+    $properties = &$info[$this->type]['properties'];
 
-    // Define extra metadata info
-    // …
+    $properties['uid']['type'] = 'user';
+    $properties['created']['type'] = 'date';
+    $properties['created']['setter callback'] = 'entity_property_verbatim_set';
+    $properties['created']['setter permission'] = 'administer quiz questions';
+    $properties['changed']['type'] = 'date';
+    $properties['changed']['setter callback'] = 'entity_property_verbatim_set';
+    $properties['changed']['setter permission'] = 'administer quiz questions';
 
     return $info;
   }
