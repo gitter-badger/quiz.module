7cbbac5f356805ae552d1fc8d27e10d06c9aa4d9
Andy Truong <thehongtt@gmail.com>
2014-11-17 16:02:28 +0700

    Return hint for quiz_question_get_types().

--- modules/quiz_question/quiz_question.todo.php
+++ modules/quiz_question/quiz_question.todo.php
@@ -24,6 +24,8 @@ function quiz_question_permisison() {
 
 /**
  * Get all question types.
+ *
+ * @return \Drupal\quiz_question\Entity\QuestionType[]
  */
 function quiz_question_get_types() {
   return entity_load_multiple_by_name('quiz_question_type');
